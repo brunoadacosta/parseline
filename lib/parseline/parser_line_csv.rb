cf0177e0ea6dd9b19ee8db019eb3e0aab2cc8e8b
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2017-05-29 13:43:20 -0300

    use File instance

--- lib/parseline/parser_line_csv.rb
+++ lib/parseline/parser_line_csv.rb
@@ -2,14 +2,14 @@
 #http://www.hashcode.eti.br
 
 module ParseLine::CSV
-  
+
   self.class_eval do
-    
+
     def csv_layout(options={:delimiter => ';'},&block)
       self.send(:class_variable_set,:@@parse_delimiter,Regexp.new(options[:delimiter])) unless self.class_variable_defined?(:@@parse_values)
       yield self
     end
-     
+
     def parse_field(field,block=nil)
       self.send(:class_variable_set,:@@parse_values,[]) unless self.class_variable_defined?(:@@parse_values)
       self.send(:class_variable_get,:@@parse_values) << [field,block]
@@ -18,23 +18,26 @@ module ParseLine::CSV
     def field(field,block=nil)
       parse_field(field,block)
     end
+
     def ignore_field
-      
       parse_field(:__ignored__)
     end
-    
+
     def load_lines(filepath)
-      
-      File.open(filepath).map{|line| load_line(line)}
-        
+      if filepath.class == File
+        filepath.map { |line| load_line(line) }
+      else
+        File.open(filepath).map{ |line| load_line(line) }
+      end
     end
+
     def load_line(line)
       begin
         this=self.new
         #puts this.inspect
         bits_line=line.to_s.split(self.send(:class_variable_get,:@@parse_delimiter)).flatten
-          
-          
+
+
         self.send(:class_variable_get,:@@parse_values).each_with_index do |field,i|
           if field[0] != :__ignored__
             if field[1] # it's block
@@ -44,7 +47,7 @@ module ParseLine::CSV
             end
           else
           end
-        
+
         end
         #puts this.inspect
         this
@@ -53,7 +56,7 @@ module ParseLine::CSV
       end
       #puts bits_line
     end
-    
+
   end
-  
+
 end
